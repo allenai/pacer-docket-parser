e9203ca5752e756b52b13b28609f75e05eced62b
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2021-03-24 12:04:58 -0700

    more robust main function

--- main.py
+++ main.py
@@ -75,24 +75,30 @@ if __name__ == "__main__":
     table_detector = TableDetector(model, pdf_extractor)
     page_parser = PageStructureParser()
 
-    for filename in args.filename:
-        print(f"Processing {filename}")
-        tables = table_detector.detect_tables_from_pdf(filename)
-        hyperlink_tokens = pdf_extractor.pdf_extractor.extract(filename, target_data="hyperlink")
-        df = collect_genererated_table_data(tables, hyperlink_tokens)
+    pbar = tqdm(args.filename)
+    for filename in pbar:
+        pbar.set_description(f"Processing {filename}")
+        try:
+            tables = table_detector.detect_tables_from_pdf(filename)
+            hyperlink_tokens = pdf_extractor.pdf_extractor.extract(filename, target_data="hyperlink")
+            df = collect_genererated_table_data(tables, hyperlink_tokens)
 
-        all_plantiffs_blocks = page_parser.parse_page_structure(filename=filename, table_regions=tables)
-        all_plantiffs_data = collect_all_plantiff_data(all_plantiffs_blocks)
+            all_plantiffs_blocks = page_parser.parse_page_structure(filename=filename, table_regions=tables)
+            all_plantiffs_data = collect_all_plantiff_data(all_plantiffs_blocks)
 
-        case_json = raw_text_parse(filename)
-        case_json['docket'] = df.apply(lambda row: row.to_list(), axis=1).to_list()
-        case_json.update(all_plantiffs_data)
+            case_json = raw_text_parse(filename)
+            case_json['docket'] = df.apply(lambda row: row.to_list(), axis=1).to_list()
+            case_json.update(all_plantiffs_data)
 
-        table_save_name = filename.replace(".pdf", ".csv")
-        df.to_csv(table_save_name, index=None)
-        print(f"Saved to {table_save_name}")
+            table_save_name = filename.replace(".pdf", ".csv")
+            df.to_csv(table_save_name, index=None)
+            pbar.set_description(f"Saved to {table_save_name}")
 
-        json_save_name = filename.replace(".pdf", ".json")
-        with open(json_save_name, 'w') as fp:
-            json.dump(case_json, fp, indent=4)
-        print(f"Saved to {json_save_name}")
+            json_save_name = filename.replace(".pdf", ".json")
+            with open(json_save_name, 'w') as fp:
+                json.dump(case_json, fp, indent=4)
+            pbar.set_description(f"Saved to {json_save_name}")
+        except KeyboardInterrupt:
+            exit()
+        except:
+            print(f"Serious issue for {filename}")
