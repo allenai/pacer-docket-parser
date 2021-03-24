7fca5a9a7974c28811bc41972acabe6631280dd2
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2021-03-24 13:07:20 -0400

    detects hyperlink in docket

--- main.py
+++ main.py
@@ -20,17 +20,40 @@ def collect_all_plantiff_data(all_plantiffs_blocks):
     return all_plantiff_data
 
 
-def collect_genererated_table_data(tables):
-    all_dfs = [
-        table.to_dataframe()
-        for table in sum([tb for tb in tables.values() if tb != []], [])
-    ]
+def collect_genererated_table_data(tables, hyperlink_tokens):
+    all_table_df = []
+    for page_id, table in tables.items():
+        if len(table)==0: continue
+            
+        if len(table)>1: print(f"Warning: page {page_id} has {len(table)} tables)")
+        table = table[0] # we assume there's only one table per page
+        table_df = table.to_dataframe()
+        
+        hyperlinks = hyperlink_tokens[page_id]["tokens"]
+        
+        if len(hyperlinks)==0: 
+            table_df[3] = None 
+            continue
+        else:
+            all_possible_links = [None]* len(table.rows)
+            for idx, rows in enumerate(table.grid):
+                col = rows[1]
+                all_possible_links[idx] = ' '.join(hyperlinks.filter_by(col, center=True).get_texts())
+            table_df[3] = all_possible_links
+        all_table_df.append(table_df)
+        
+    combined_df = all_table_df[0]
+    for table_df in all_table_df[1:]:
+        if table_df.iloc[0,0]!='':
+            combined_df = combined_df.append(table_df)
+        else:
+            combined_df.iloc[-1, 2] += ' ' + table_df.iloc[0, 2] # combine text
+            combined_df.iloc[-1, 3] += table_df.iloc[0, 3] # combine link
+            combined_df = combined_df.append(table_df.iloc[1:])
 
-    columns = all_dfs[0].iloc[0, :].tolist()
-    all_dfs[0].drop([0], inplace=True)
-    df = pd.concat(all_dfs)
-    df.columns = columns
-    return df
+    combined_df.iloc[0, 3] = "URL"
+    combined_df.columns = combined_df.iloc[0].tolist()
+    return combined_df.drop([0])
 
 
 if __name__ == "__main__":
@@ -55,13 +78,14 @@ if __name__ == "__main__":
     for filename in args.filename:
         print(f"Processing {filename}")
         tables = table_detector.detect_tables_from_pdf(filename)
-        df = collect_genererated_table_data(tables)
+        hyperlink_tokens = pdf_extractor.pdf_extractor.extract(filename, target_data="hyperlink")
+        df = collect_genererated_table_data(tables, hyperlink_tokens)
 
         all_plantiffs_blocks = page_parser.parse_page_structure(filename=filename, table_regions=tables)
         all_plantiffs_data = collect_all_plantiff_data(all_plantiffs_blocks)
 
         case_json = raw_text_parse(filename)
-        case_json['docket'] = df.apply(lambda row: row.to_list() + [{}], axis=1).to_list()
+        case_json['docket'] = df.to_list()
         case_json.update(all_plantiffs_data)
 
         table_save_name = filename.replace(".pdf", ".csv")
