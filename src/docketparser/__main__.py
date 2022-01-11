23f7a56b609bff50637229800b5bb470db4d934e
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2022-01-11 13:50:33 -0500

    Update __main__.py

--- src/docketparser/__main__.py
+++ src/docketparser/__main__.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 @click.group(context_settings={"help_option_names": ["--help", "-h"]})
 @click.option("--verbose", "-v", is_flag=True, help="Set log level to DEBUG.")
 def docketparser_cli(verbose):
-    """An interface for managing PDFs for PAWLS."""
+    """Parsing structured information from PACER Dockets."""
     log_level = logging.DEBUG if verbose else logging.INFO
     logger.setLevel(log_level)
 
